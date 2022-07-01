674eaf17d07f6b53f10a8aa646cdaaeafbdd7abb
k.ryizhih <k.ryizhih@fait.gl>
2022-07-01 12:31:08 +0300

    change script

--- checker.sh
+++ checker.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # Website status checker. by ET (etcs.me)
 
-WORKSPACE=/scripts/isOnline
+WORKSPACE=.
 # list of websites. each website in new line. leave an empty line in the end.
 LISTFILE=$WORKSPACE/websites.lst
 # Send mail in case of failure to. leave an empty line in the end.
 EMAILLISTFILE=$WORKSPACE/emails.lst
 # Temporary dir
 TEMPDIR=$WORKSPACE/cache
+TIME=$(date '+%d/%m %H:%M')
 
 # `Quiet` is true when in crontab; show output when it's run manually from shell.
 # Set THIS_IS_CRON=1 in the beginning of your crontab -e.
@@ -19,16 +20,16 @@ function test {
   filename=$( echo $1 | cut -f1 -d"/" )
   if [ "$QUIET" = false ] ; then echo -n "$p "; fi
 
-  if [ $response -eq 200 ] ; then
+  if [ $response -eq 403 ] ; then
     # website working
     if [ "$QUIET" = false ] ; then
-      echo -n "$response "; echo -e "\e[32m[ok]\e[0m"
+      echo -n "$TIME $response "; echo -e "\e[32m[ok]\e[0m"
     fi
     # remove .temp file if exist 
     if [ -f $TEMPDIR/$filename ]; then rm -f $TEMPDIR/$filename; fi
   else
     # website down
-    if [ "$QUIET" = false ] ; then echo -n "$response "; echo -e "\e[31m[DOWN]\e[0m"; fi
+    if [ "$QUIET" = false ] ; then echo -n "$TIME $response "; echo -e "\e[31m[DOWN]\e[0m"; fi
     if [ ! -f $TEMPDIR/$filename ]; then
         while read e; do
             # using mailx command
